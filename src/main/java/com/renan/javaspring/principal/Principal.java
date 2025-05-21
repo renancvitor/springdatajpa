bc4ed7e1ce8a5934d6dfd7d9e85ce7cbae8e00a6
renancvitor <renan.vitor.cm@gmail.com>
2025-05-21 14:52:53 -0300

    Busca por trecho do título

--- src/main/java/com/renan/javaspring/principal/Principal.java
+++ src/main/java/com/renan/javaspring/principal/Principal.java
@@ -37,7 +37,8 @@ public class Principal {
             var menu = """
                     1 - Buscar séries
                     2 - Buscar episódios.
-                    3 - Listar séries buscadas  
+                    3 - Listar séries buscadas
+                    4 - Buscar série por título
                                     
                     0 - Sair
                     """;
@@ -55,6 +56,9 @@ public class Principal {
                     break;
                 case 3:
                     listarSeriesBuscadas();
+                    break;
+                case 4:
+                    buscarSeriePorTitulo();
                     break;
                 case 0:
                     System.out.println("Saindo...");
@@ -87,9 +91,7 @@ public class Principal {
         System.out.println("Escolha uma série pelo nome: ");
         var nomeSerie = scan.nextLine();
 
-        Optional<Serie> serie = series.stream()
-                .filter(s -> s.getTitulo().toLowerCase().contains(nomeSerie.toLowerCase()))
-                .findFirst();
+        Optional<Serie> serie =  repository.findByTituloContainingIgnoreCase(nomeSerie);
 
         if (serie.isPresent()) {
 
@@ -121,5 +123,17 @@ public class Principal {
         series.stream()
                 .sorted(Comparator.comparing(Serie::getGenero))
                 .forEach(System.out::println);
+    }
+
+    private void buscarSeriePorTitulo() {
+        System.out.println("Escolha uma série pelo nome: ");
+        var nomeSerie = scan.nextLine();
+        Optional<Serie> serieBuscada = repository.findByTituloContainingIgnoreCase(nomeSerie);
+
+        if (serieBuscada.isPresent()) {
+            System.out.println("Dados da série: " + serieBuscada.get());
+        } else {
+            System.out.println("Série não encontrada.");
+        }
     }
 }
--- src/main/java/com/renan/javaspring/repository/SerieRepository.java
+++ src/main/java/com/renan/javaspring/repository/SerieRepository.java
@@ -3,5 +3,8 @@ package com.renan.javaspring.repository;
 import com.renan.javaspring.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SerieRepository extends JpaRepository<Serie, Long> {
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
 }
