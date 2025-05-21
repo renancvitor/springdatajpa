59efea77471228d0f829cf2a402514188141d698
renancvitor <renan.vitor.cm@gmail.com>
2025-05-21 14:15:52 -0300

    Ajustando a chave estrangeira

--- src/main/java/com/renan/javaspring/model/Serie.java
+++ src/main/java/com/renan/javaspring/model/Serie.java
@@ -23,7 +23,7 @@ public class Serie {
     private String poster;
     private String sinops;
 
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie(DadosSerie dadosSerie) {
@@ -53,6 +53,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -122,6 +123,7 @@ public class Serie {
 
                 ", Atores='" + atores + '\'' +
                 ", Poster='" + poster + '\'' +
-                ", Sinops='" + sinops + '\'';
+                ", Sinops='" + sinops + '\'' +
+                ", Epsódios='" + episodios + '\'';
     }
 }
