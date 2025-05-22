e139dd06d3f25e993a6491e21f42c692b3d94eda
renancvitor <renan.vitor.cm@gmail.com>
2025-05-22 13:29:33 -0300

    Correção simples

--- src/main/java/com/renan/javaspring/apiScreenSound/modelSound/AlbumSimples.java
+++
@@ -1,7 +0,0 @@
-package com.renan.javaspring.apiScreenSound.modelSound;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public record AlbumSimples(
-        @JsonAlias("title") String titulo
-) {}
--- src/main/java/com/renan/javaspring/apiScreenSound/modelSound/ArtistaSimples.java
+++
@@ -1,7 +0,0 @@
-package com.renan.javaspring.apiScreenSound.modelSound;
-
-import com.fasterxml.jackson.annotation.JsonAlias;
-
-public record ArtistaSimples(
-        @JsonAlias("name") String nome
-) {}
